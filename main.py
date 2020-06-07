97777dcc7868cd693512d60c2b2019ae44d1aa22
Ameet Porwal <ameet.porwal@citrix.com>
2020-06-07 19:51:19 +0530

    main function for calc app

---
+++ main.py
@@ -0,0 +1,11 @@
+import sys
+x = int(raw_input("enter numeric value for y :")
+y = int(raw_input("enter numeric value for y :")
+
+choice = int(raw_input("Enter the operataion: \
+        1. Press 1 for addition \
+        2. Press 2 for Subtraction")
+if choice == 1:
+    add(x,y)
+else:
+    subtract(x,y)
