21a947e53a46c9f94eda413c3004f81800c4c79c
Ameet Porwal <ameet.porwal@citrix.com>
2020-06-07 20:19:59 +0530

    More functionality added

--- calc.py
+++ calc.py
@@ -1,9 +1,10 @@
 def add(x, y):
     print ("sum: ", x+y)
 def substract(x,y):
-    pass
+    print ("Substract: ", x-y)
 def multiply(x,y):
-    pass
+    print  ("Multiplication :", x *y)
 def division(x,y):
-    pass
+    if y!=0:
+        print ("Division: ", x/y)
 
